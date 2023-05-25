34fe3f4d1fea83835ac0a7ee57bf4490f5bd7ae5
ayomahmusah <ayomahmubarak7@gamil.com>
2023-05-24 18:56:52 -0700

    exit code

--- _exit.c
+++ _exit.c
@@ -1,31 +1,74 @@
 #include "main.h"
 
 /**
- * exit_shell - exits the shell
- *
- * @datash: data relevant (status and args)
- * Return: 0 on success.
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
  */
-int exit_shell(data_shell *datash)
+char *_strncpy(char *dest, char *src, int n)
 {
-	unsigned int ustatus;
-	int is_digit;
-	int str_len;
-	int big_number;
+	int i, j;
+	char *s = dest;
 
-	if (datash->args[1] != NULL)
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
 	{
-		ustatus = _atoi(datash->args[1]);
-		is_digit = _isdigit(datash->args[1]);
-		str_len = _strlen(datash->args[1]);
-		big_number = ustatus > (unsigned int)INT_MAX;
-		if (!is_digit || str_len > 10 || big_number)
-		{
-			get_error(datash, 2);
-			datash->status = 2;
-			return (1);
-		}
-		datash->status = (ustatus % 256);
+		dest[i] = src[i];
+		i++;
 	}
-	return (0);
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
 }
